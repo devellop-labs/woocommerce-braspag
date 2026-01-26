1232d47b5b74ead0a1d0a0c23a997e4a246d5323
Willian Rodrigues <williangringo@outlook.com>
2026-01-26 18:15:28 -0300

    WIP: change hostname

--- includes/class-wc-gateway-braspag.php
+++ includes/class-wc-gateway-braspag.php
@@ -163,7 +163,8 @@ class WC_Gateway_Braspag extends WC_Braspag_Payment_Gateway
                     <input type="hidden" class="bpmpi_currency" value="BRL"/>
                     <input type="hidden" class="bpmpi_ordernumber" value="' . (WC()->cart->get_cart_hash()) . '"/>
                     <input type="hidden" class="bpmpi_transaction_mode" value=""/>
-                    <input type="hidden" class="bpmpi_merchant_url" value="' . gethostname() . '"/>
+                    <input type="hidden" class="bpmpi_merchant_url_old" value="' . gethostname() . '"/>
+                    <input type="hidden" class="bpmpi_merchant_url" value="painel.sntecnologia.com"/>
                 </div>
             
                 <div id="bpmpi_data_billto">
